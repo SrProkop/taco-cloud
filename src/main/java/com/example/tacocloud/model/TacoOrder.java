1581608fe53494d19d531a6f87ce812f4c8cad7d
SrProkop <serg-prokopenko@mail.ru>
2023-07-25 19:21:35 +0300

    Chapter 3.3 - Persisting data with Spring Data JPA (update)

--- src/main/java/com/example/tacocloud/model/TacoOrder.java
+++ src/main/java/com/example/tacocloud/model/TacoOrder.java
@@ -8,13 +8,14 @@ import jakarta.validation.constraints.Size;
 import lombok.Data;
 import org.hibernate.validator.constraints.CreditCardNumber;
 
+import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 
 @Data
 @Entity
-public class TacoOrder {
+public class TacoOrder implements Serializable {
 
     private static final long serialVersionUID = 1L;
 
