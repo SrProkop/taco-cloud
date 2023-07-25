7d02b7b3faf6be09f5cc688ad3c3918b67e722eb
SrProkop <serg-prokopenko@mail.ru>
2023-07-25 19:18:46 +0300

    Chapter 3.3 - Persisting data with Spring Data JPA

--- src/main/java/com/example/tacocloud/repository/OrderRepository.java
+++ src/main/java/com/example/tacocloud/repository/OrderRepository.java
@@ -1,7 +1,25 @@
 package com.example.tacocloud.repository;
 
 import com.example.tacocloud.model.TacoOrder;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.Date;
+import java.util.List;
+
 public interface OrderRepository extends CrudRepository<TacoOrder, Long> {
+
+    List<TacoOrder> findByDeliveryZip(String deliveryZip);
+
+    List<TacoOrder> readOrdersByDeliveryZipAndPlacedAtBetween(
+            String deliveryZip, Date startDate, Date endDate);
+
+    List<TacoOrder> findByDeliveryZipAndDeliveryCityIgnoreCase(
+            String deliveryTo, String deliveryCity);
+
+    List<TacoOrder> findByDeliveryCityOrderByDeliveryZip(String city);
+
+    @Query(value = "Order o where o.deliveryCity='Seattle'", nativeQuery = true)
+    List<TacoOrder> readOrdersDeliveredInSeattle();
+
 }
