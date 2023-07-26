48c9792d73da65535d929372eaf3ebae91c0ad3f
SrProkop <serg-prokopenko@mail.ru>
2023-07-26 15:10:53 +0300

    Chapter 5.3 - Web request protection

--- src/main/java/com/example/tacocloud/config/SecurityConfig.java
+++ src/main/java/com/example/tacocloud/config/SecurityConfig.java
@@ -3,10 +3,12 @@ package com.example.tacocloud.config;
 import com.example.tacocloud.repository.UserRepository;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
 
 
 @Configuration
@@ -26,4 +28,19 @@ public class SecurityConfig {
         };
     }
 
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        return http
+                .authorizeHttpRequests()
+                .requestMatchers("/design", "/orders").hasRole("USER")
+                .requestMatchers("/", "/**").permitAll()
+                .and()
+                .formLogin()
+                .loginPage("/login")
+                .defaultSuccessUrl("/design")
+                .and()
+                .logout()
+                .and()
+                .build();
+    }
 }
