14b129e041a217e37bbbcde36b1c16890763b9cb
SrProkop <serg-prokopenko@mail.ru>
2023-08-03 23:14:43 +0300

    Chapter 7.1 - Create controllers RESTful

--- src/main/java/com/example/tacocloud/repository/TacoRepository.java
+++ src/main/java/com/example/tacocloud/repository/TacoRepository.java
@@ -1,7 +1,7 @@
 package com.example.tacocloud.repository;
 
 import com.example.tacocloud.model.Taco;
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.PagingAndSortingRepository;
 
-public interface TacoRepository extends CrudRepository<Taco, Long> {
+public interface TacoRepository extends PagingAndSortingRepository<Taco, Long> {
 }
